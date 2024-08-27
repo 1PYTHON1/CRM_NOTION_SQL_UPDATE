9918dc015a9accec88cc02f9801012868c75fdda
José Illatopa Daza <joseillatopadaza@gmail.com>
2024-08-26 19:22:54 -0500

    CON DATOS SQL SERVER

--- REGISTRADOR TEMP EXP EXCEL CON CONFIRMACION.py
+++ REGISTRADOR TEMP EXP EXCEL CON CONFIRMACION.py
@@ -229,7 +229,7 @@ class DataEntryWidget(QWidget):
     
         try:
             # Verificar si la OP ya existe en la tabla CLIENTES
-            self.cursor.execute("SELECT COUNT(*) FROM CLIENTES WHERE OP = ?", (OP,))
+            self.cursor.execute("SELECT COUNT(*) FROM CLIENTES_TEMPLADOS WHERE OP = ?", (OP,))
             op_count = self.cursor.fetchone()[0]
     
             if op_count > 0:
@@ -237,7 +237,7 @@ class DataEntryWidget(QWidget):
                 return
     
             # Insertar el nuevo cliente si la OP no existe
-            self.cursor.execute("INSERT INTO CLIENTES VALUES (?, ?, ?, ?, ?, ?, ?, ?, 'agregado')", (OP, cliente, vidrio, espesor, fecha, fecha_entrega, total, enjabado))
+            self.cursor.execute("INSERT INTO CLIENTES_TEMPLADOS VALUES (?, ?, ?, ?, ?, ?, ?, ?, 'agregado')", (OP, cliente, vidrio, espesor, fecha, fecha_entrega, total, enjabado))
             self.conn.commit()
             QMessageBox.information(self, 'Éxito', 'Datos del cliente agregados correctamente.')
         except Exception as e:
@@ -308,7 +308,7 @@ class DataEntryWidget(QWidget):
             
                 try:
                     # Verificar si la OP ya existe en la tabla CLIENTES
-                    self.cursor.execute("SELECT COUNT(*) FROM CLIENTES WHERE OP = ?", (op,))
+                    self.cursor.execute("SELECT COUNT(*) FROM CLIENTES_TEMPLADOS WHERE OP = ?", (op,))
                     op_count = self.cursor.fetchone()[0]
             
                     if op_count > 0:
@@ -316,7 +316,7 @@ class DataEntryWidget(QWidget):
                         return
             
                     # Insertar los datos del cliente en la tabla CLIENTES
-                    self.cursor.execute("INSERT INTO CLIENTES VALUES (?, ?, ?, ?, ?, ?, ?, ?, 'agregado')",
+                    self.cursor.execute("INSERT INTO CLIENTES_TEMPLADOS VALUES (?, ?, ?, ?, ?, ?, ?, ?, 'agregado')",
                                         (op, cliente, vidrio, espesor, fecha, fecha_entrega, total, enjabado))
                     self.conn.commit()
             
@@ -329,7 +329,7 @@ class DataEntryWidget(QWidget):
                         values = item_text.split(',')
                         op = values[0].split(':')[1].strip()
                         item_num = values[1].split(':')[1].strip()
-                        self.cursor.execute("SELECT COUNT(*) FROM PEDIDOS WHERE OP = ? AND ITEM = ?", (op, item_num))
+                        self.cursor.execute("SELECT COUNT(*) FROM PEDIDOS_TEMPLADOS WHERE OP = ? AND ITEM = ?", (op, item_num))
                         duplicate_count = self.cursor.fetchone()[0]
                         if duplicate_count > 0:
                             QMessageBox.warning(self, 'Error', 'Ya existe una entrada con la misma OP y el mismo item en la tabla PEDIDOS.')
@@ -350,7 +350,7 @@ class DataEntryWidget(QWidget):
                         comentario = values[8].split(':')[1].strip()
             
                         # Insertar los valores en la tabla de PEDIDOS
-                        self.cursor.execute("INSERT INTO PEDIDOS (OP, ITEM, BASE, ALTURA, ENTALLE, LAMINADO, PINTADO, INSULADO, COMENTARIO) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                        self.cursor.execute("INSERT INTO PEDIDOS_TEMPLADOS (OP, ITEM, BASE, ALTURA, ENTALLE, LAMINADO, PINTADO, INSULADO, COMENTARIO) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
                                             (op, item_num, base, altura, entalle, laminado, pintado, insulado, comentario))
                         self.conn.commit()
             
@@ -435,7 +435,7 @@ class DataEntryWidget(QWidget):
         try:
             DETECTOR_cursor = connection.cursor()
             DETECTOR_cursor.execute("SELECT @@version;")
-            DETECTOR_query = 'SELECT * FROM PT_V2 where OP = '+ OP
+            DETECTOR_query = 'SELECT * FROM PEDIDOS_TEMPLADOS where OP = '+ OP
             DETECTOR_cursor.execute(DETECTOR_query)
             DETECTOR_FILAS = DETECTOR_cursor.fetchall()
             filas_OP = len(DETECTOR_FILAS)
